cac1ce82653cfd9064fa074757873a6715dcd205
denalves <50816778+denalves@users.noreply.github.com>
2021-08-02 19:04:54 -0300

    iniciando projeto repos

---
+++ src/pages/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {FaGithub, FaPlus} from 'react-icons/fa'
+import {Container, Form, SubmitButton} from './styles';
+
+const Main = () => {
+    return (
+        <div>
+            <Container>
+               <h1>
+                   <FaGithub size={25}/>
+                   Meus Repositorios
+                </h1>
+                
+                <Form onSubmit={() => {}}>
+                    <input type="text" placeholder="Adicionar Respositorios" />
+
+                    <SubmitButton>
+                        <FaPlus color="#fff" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/pages/Repositorio/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repositorio = () => {
+    return (
+        <div>
+            <h1>Repositorrio</h1>
+        </div>
+    )
+}
+
+export default Repositorio;
