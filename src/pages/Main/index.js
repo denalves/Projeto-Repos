8db43ab5a19954caa15d14ba7dcc74e50a769d01
denalves <50816778+denalves@users.noreply.github.com>
2021-08-05 20:08:22 -0300

    listando repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from "react";
-import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
-import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
+import { Container, Form, SubmitButton, List, DeleteButton } from "./styles";
 
 import api from "../../services/api";
 
@@ -9,36 +9,41 @@ const Main = () => {
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = useCallback((e) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-
-    async function submit() {
+      async function submit() {
         setLoading(true);
-        try{
-            const response = await api.get(`repos/${newRepo}`);
+        try {
+          const response = await api.get(`repos/${newRepo}`);
 
- 
-            const data = {
-              name: response.data.full_name,
-            }
-      
-            setRepositorios([...repositorios, data]);
-            setNewRepo("");
-          }catch(error){
-              console.log(error);
-          }finally{
-            setLoading(false);
-          }
+          const data = {
+            name: response.data.full_name,
+          };
+
+          setRepositorios([...repositorios, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
         }
+      }
 
-
-    submit();
-  }, [newRepo, repositorios]);
+      submit();
+    },
+    [newRepo, repositorios]
+  );
 
   function handleinputChange(e) {
     setNewRepo(e.target.value);
   }
+
+  const handleDelete = useCallback((repo) => {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  },[repositorios])
 
   return (
     <div>
@@ -57,13 +62,29 @@ const Main = () => {
           />
 
           <SubmitButton loading={loading ? 1 : 0}>
-              {loading ? (
-                  <FaSpinner color="#fff" size={14} />
-              ) : (
-                  <FaPlus color="#FFF" size={14} />
-              )}
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositorios.map((repo) => (
+            <li key={repo.name}>
+              <span>
+                <DeleteButton onClick={() => handleDelete(repo.name)}>
+                    <FaTrash size={14} />
+                </DeleteButton>
+                {repo.name}
+              </span>
+              <a href="">
+                <FaBars size={20} />
+              </a>
+            </li>
+          ))}
+        </List>
       </Container>
     </div>
   );
